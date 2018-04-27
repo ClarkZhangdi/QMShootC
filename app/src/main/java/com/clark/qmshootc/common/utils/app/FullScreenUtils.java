9c2e734b7d6a0bfed6b0c5015b2df402bc0cffe2
Jerome Zhang <1017397593@qq.com>
2018-04-27 23:33:05 +0800

    完成login界面部分功能

--- app/src/main/java/com/clark/qmshootc/common/utils/app/FullScreenUtils.java
+++ app/src/main/java/com/clark/qmshootc/common/utils/app/FullScreenUtils.java
@@ -17,6 +17,47 @@ import static com.amiba.frame.androidframe.util.PhoneUtils.getStatusBarHeight;
  */
 public class FullScreenUtils {
 
+
+    /**
+     * 设置Activity的statusBar隐藏
+     *
+     * @param activity
+     */
+    public static void statusBarHide(AppCompatActivity activity) {
+        // 代表 5.0 及以上
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            View decorView = activity.getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+            decorView.setSystemUiVisibility(option);
+            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+            return;
+        }
+
+        // versionCode > 4.4  and versionCode < 5.0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+    }
+
+    /**
+     * 设置是否隐藏导航栏
+     *
+     * @param activity
+     * @param
+     */
+    public static void setNavigationBar(AppCompatActivity activity, int visible) {
+        View decorView = activity.getWindow().getDecorView();
+        //显示NavigationBar
+        if (View.GONE == visible) {
+            int option = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+            decorView.setSystemUiVisibility(option);
+        }
+    }
+
+
+
     /**
      * 该方法只能在onWindowFocusChange方法中调用
      * 确认当前模式为全屏状态
--- app/src/main/java/com/clark/qmshootc/modules/login/components/LoginActivity.java
+++ app/src/main/java/com/clark/qmshootc/modules/login/components/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.clark.qmshootc.modules.login.components;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 
 import com.amiba.frame.androidframe.base.BaseActivity;
+import com.amiba.frame.androidframe.ui.sysbar.SystemBarTintUtil;
 import com.clark.qmshootc.R;
+import com.clark.qmshootc.common.utils.app.FullScreenUtils;
 
 public class LoginActivity extends BaseActivity {
 
@@ -15,5 +20,6 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FullScreenUtils.FullScreen(this);
     }
 }
